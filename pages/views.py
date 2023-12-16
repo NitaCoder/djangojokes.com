e0012649b2c07fdac7ffa6f1c17088802915ec8d
Nita Shutt <nita.madison@live.com>
2023-12-16 10:54:39 -0700

    added messages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
@@ -6,3 +7,4 @@ class HomePageView(TemplateView):
 
 class AboutUsView(TemplateView):
     template_name = 'pages/about_us.html'
+
